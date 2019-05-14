e0ebe3e5acac188f3c874e93f016d4e2c29f4160
Thomas Graeff <676@holbertonschool.com>
2019-05-14 20:31:46 +0000

    Update monty.h

--- monty.h
+++ monty.h
@@ -35,5 +35,7 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+void _exit(stack_t *stack);
+void free_stack(stack_t *stack);
 
 #endif /* MONTY_H */
