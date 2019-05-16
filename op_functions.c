0ed0c67b3c6cff7b44797dd6a65f36874f0988fa
sungnga <708@holbertonschool.com>
2019-05-16 02:26:45 +0000

    Initial commit

---
+++ op_functions.c
@@ -0,0 +1,68 @@
+#include "monty.h"
+
+/**
+ * opcode_push - adds a new node at the end of a stack_t list
+ * @head: pointer to the address of first node in a linked list
+ * @num: pointer to second argument in a line number
+ * Return: nothing
+ */
+void opcode_push(stack_t **head, char *num)
+{
+	stack_t *new_node;
+	int n = 0;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		free_everything();
+		exit_helper(4, NULL);
+	}
+	for (i = 0; num[i]; i++)
+	{
+		if (num[i] == "-")
+			i++;
+		if (num[i] < '1' || num[i] > '9')
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", gl.ln);
+			free_everything();
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	n = atoi(num);
+
+	if (strlen(num) == 0)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", gl.ln);
+		free_everything();
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	n = atoi(num);
+
+	if (*head != NULL)
+		(*head)->prev = new_node;
+
+	*head = new_node;
+}
+
+/**
+ * opcode_pall - prints all the elements of a stack_t list
+ * @stack: pointer to the address of node in stack_t list
+ * @line_number: line number in the file
+ * Return: nothing
+ */
+void opcode_pall(stack_t **stack, unsigned int line_number)
+{
+	(void) line_number;
+	const stack_t *temp;
+
+	temp = (*stack);
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+}
