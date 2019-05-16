f5fca087ff8bc2d5b7ef9989ba58588c019ee7a8
sungnga <708@holbertonschool.com>
2019-05-16 22:26:55 +0000

    Implement add opcode function

--- monty.h
+++ monty.h
@@ -63,6 +63,7 @@ void opcode_pall(stack_t **stack, unsigned int line_number);
 void opcode_pint(stack_t **stack, unsigned int line_number);
 void opcode_pop(stack_t **stack, unsigned int line_number);
 void opcode_swap(stack_t **stack, unsigned int line_number);
+void opcode_add(stack_t **stack, unsigned int line_number);
 void opcode_nop(stack_t **stack, unsigned int line_number);
 void initialize(void);
 void exit_helper(int code, char *file);
