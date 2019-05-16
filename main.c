17cb43a6a1eb6c365e24e3f8920be8a7178b6444
sungnga <708@holbertonschool.com>
2019-05-16 20:23:55 +0000

    Fix spelling error for swap function prototype

--- main.c
+++ main.c
@@ -67,7 +67,7 @@ int get_op(stack_t **stack, char *which)
 		{"pall", opcode_pall},
 		{"pint", opcode_pint},
 		{"pop", opcode_pop},
-		{"swap", optcode_swap},
+		{"swap", opcode_swap},
 		{"nop", opcode_nop},
 		{NULL, NULL}
 		};
