485933f5f05f69cfb2a50bfa242b9d54d47bbab3
sungnga <708@holbertonschool.com>
2019-05-16 19:12:30 +0000

    Split op_functions.c file into two files

---
+++ op_2.c
@@ -0,0 +1,11 @@
+/**
+ * opcode_nop - does not perform the instruction
+ * @stack: pointer to the address of node in stack_t list
+ * @line_number: line number in the file
+ * Return: nothing
+ */
+void opcode_nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
